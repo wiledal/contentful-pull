4b536748b9ebcfb2eb37b1a62b42034a4f52ee0e
Hugo Wiledal <hugo.wiledal@gmail.com>
2016-06-14 17:20:30 +0200

    improved sync script

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function throwError(err) {
 */
 function ContentfulPull(settings) {
   this.currentSyncToken = null;
-  
+
   // Holds all the downloaded data in memory
   this.data = null;
-  
+
   this.defaultSettings = {
     path: null,
     space: null,
@@ -30,32 +30,32 @@ function ContentfulPull(settings) {
 */
 ContentfulPull.prototype.sync = function(options) {
   var _this = this;
-  
+
   console.log("ContentfulPull | Syncing...");
-  
+
   var d = Promise.defer();
-  
+
   var client = contentful.createClient({
     space: this.settings.space,
     accessToken: this.settings.accessToken
   })
-  
+
   var isInitial = this.currentSyncToken ? false : true;
-  
+
   var spacePromise = client.getSpace();
   var contentTypesPromise = client.getContentTypes();
   var syncPromise = client.sync({initial: isInitial, resolveLinks: false, nextSyncToken: this.currentSyncToken});
-  
+
   Promise.all([spacePromise, contentTypesPromise, syncPromise]).then(function(result) {
     var handledData = _this.handleSyncResponse({
       space: result[0],
       contentTypes: result[1],
       sync: result[2]
     });
-    
+
     d.resolve(_this.transformData(handledData));
   });
-  
+
   return d.promise;
 };
 
@@ -66,41 +66,43 @@ ContentfulPull.prototype.sync = function(options) {
 ContentfulPull.prototype.handleSyncResponse = function(resp) {
   var _this = this;
   this.currentSyncToken = resp.sync.nextSyncToken;
-  
+
   if (!this.data) this.data = resp;
-  
+
   // If there are updates, replace current with new
   if (resp.sync.entries) {
+    var syncIDs = _this.data.sync.entries.map(function(e) { return e.sys.id });
     resp.sync.entries.forEach(function(entry) {
-      var alreadyExist = _this.data.sync.entries.map(function(e) { return e.sys.id }).indexOf(entry.sys.id);
-      if (alreadyExist > -1) {
-        _this.data.sync.entries[alreadyExist] = entry;
+      var existID = syncIDs.indexOf(entry.sys.id);
+      if (existID > -1) {
+        _this.data.sync.entries[existID] = entry;
       }else{
         _this.data.sync.entries.push(entry);
-      }  
+      }
     })
   }
-  
+
   if (resp.sync.assets) {
+    var syncIDs = _this.data.sync.assets.map(function(e) { return e.sys.id })
     resp.sync.assets.forEach(function(asset) {
-      var alreadyExist = _this.data.sync.assets.map(function(e) { return e.sys.id }).indexOf(asset.sys.id);
-      if (alreadyExist > -1) {
-        _this.data.sync.assets[alreadyExist] = asset;
+      var existID = syncIDs.indexOf(asset.sys.id);
+      if (existID > -1) {
+        _this.data.sync.assets[existID] = asset;
       }else{
         _this.data.sync.assets.push(asset);
-      }  
+      }
     })
   }
-  
+
   // remove deleted from entries
-  if (resp.sync.deletedEntries) {    
+  if (resp.sync.deletedEntries) {
     resp.sync.deletedEntries.forEach(function(deletedEntry) {
       _this.data.sync.entries.forEach(function(entry, index) {
         if (entry.sys.id == deletedEntry.sys.id) _this.data.sync.entries.splice(index, 1);
       })
     })
   }
-  
+
   // remove deleted from assets
   if (resp.sync.deletedAssets) {
     resp.sync.deletedAssets.forEach(function(deletedAsset) {
@@ -109,7 +111,7 @@ ContentfulPull.prototype.handleSyncResponse = function(resp) {
       })
     })
   }
-  
+
   // Save to local file
   this.saveLocal(_this.data).catch(throwError);
   return this.data;
@@ -123,41 +125,41 @@ ContentfulPull.prototype.transformData = function(data, options) {
   // If RAW data is requested, return basic object
   if (!options) options = {};
   if (options.raw) return data;
-  
+
   // Else continue to make workable data
   var transformedData = {
     assets: [],
     entries: []
   };
-  
+
   data.sync.entries.forEach(function(entry) {
     var formatted = helpers.formatEntry(entry, data.contentTypes, data.space);
     transformedData.entries.push(formatted);
   });
-  
+
   data.sync.assets.forEach(function(asset) {
     var formatted = helpers.formatAsset(asset, data.contentTypes, data.space);
     transformedData.assets.push(formatted);
   });
-  
+
   // Resolves the links
   if (options.resolveLinks) {
     for (var i = 0; i < transformedData.entries.length; i++) {
       // For each entry
       var entry = transformedData.entries[i];
-      
+
       for (var key in entry.fields) {
         // Go through each field
         var field = entry.fields[key];
-        
+
         for (var lang in field) {
           // And go through each language
           var fieldContent = field[lang];
-          
+
           if (fieldContent instanceof Object && !(fieldContent instanceof Array)) {
             // If the field is an object, find the entry with the id and set it
             // to that
-            
+
             var lookArray = transformedData.entries;
             if (fieldContent.type == "asset") {
               lookArray = transformedData.assets;
@@ -172,7 +174,7 @@ ContentfulPull.prototype.transformData = function(data, options) {
       }
     }
   }
-  
+
   return transformedData;
 }
 
@@ -183,7 +185,7 @@ ContentfulPull.prototype.transformData = function(data, options) {
 ContentfulPull.prototype.getFromFile = function(options) {
   var _this = this;
   var d = Promise.defer();
-  
+
   fs.readFile(this.settings.path, "utf8", function(err, resp) {
     if (err) d.reject(err);
     if (!err) {
@@ -192,7 +194,7 @@ ContentfulPull.prototype.getFromFile = function(options) {
       d.resolve(_this.transformData(_this.data, options));
     }
   })
-  
+
   return d.promise;
 }
 
@@ -203,7 +205,7 @@ ContentfulPull.prototype.getFromFile = function(options) {
 ContentfulPull.prototype.get = function(options) {
   var _this = this;
   var d = Promise.defer();
-  
+
   // If no data is in memory, get from file, if that's not existing, sync from contentful
   if (!this.data) {
     this.getFromFile(options).then(d.resolve).catch(function() {
@@ -214,7 +216,7 @@ ContentfulPull.prototype.get = function(options) {
       resolve(_this.transformData(_this.data, options));
     })
   }
-  
+
   return d.promise;
 }
 
@@ -224,9 +226,9 @@ ContentfulPull.prototype.get = function(options) {
 */
 ContentfulPull.prototype.saveLocal = function(data) {
   var d = Promise.defer();
-  
+
   console.log("ContentfulPull | Saving to local file...");
-  
+
   // Write to local storage
   fs.writeFile(this.settings.path, JSON.stringify(data), "utf8", function(err) {
     if (err) {
@@ -238,7 +240,7 @@ ContentfulPull.prototype.saveLocal = function(data) {
       d.resolve();
     }
   });
-  
+
   return d.promise;
 };
 
